76e5e57fc40afe621e379ea4a94f53831cf58712
halilcn <halilc.2001@gmail.com>
2022-07-02 17:55:14 +0300

    add bem style

--- src/components/dashboard/content/home/menu-category/index.tsx
+++ src/components/dashboard/content/home/menu-category/index.tsx
@@ -6,48 +6,12 @@ interface IProps {}
 
 const MenuCategory: React.FC<IProps> = props => {
   return (
-    <div className="menu-category-container">
-      <div className="title">Menu Category</div>
-      <div className="menu-list">
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item selected">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
-        </div>
-        <div className="item">
-          <img className="icon" src="/icons/beef-50.png" />
-          <div className="name">Beef</div>
+    <div className="menu-category">
+      <div className="menu-category__title">Menu Category</div>
+      <div className="menu-category__list">
+        <div className="menu-category__item menu-category__item--selected">
+          <img className="menu-category__icon" src="/icons/beef-50.png" />
+          <div className="menu-category__name">Beef</div>
         </div>
       </div>
     </div>
